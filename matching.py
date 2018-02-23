a44675461e1b646073a97aa9d59094b721e5edba
ssheiba <shimon.sheiba@intel.com>
2018-02-23 16:46:04 +0200

    Class matching- start

--- matching.py
+++ matching.py
@@ -11,124 +11,172 @@ import matplotlib.pyplot as plt
 import os
 
 
-def estimate_propensity(formula, data, show_summary=False):
-    """ Estimates a propensity model using generalized linear models.
+class Matching():
 
-    Arguments:
-    ----------
-        formula:       String that contains the (R style) model formula.
-        data:          DataFrame containing the covariates for each observation.
-        show_summary:  Boolean that indicates to print out the model summary to the console.
-    """
-    model = smf.glm(formula=formula, data=data, family=sma.families.Binomial()).fit()
+    def __init__(self):
 
-    if show_summary:
-        print(model.summary())
+        self.base_directory = os.path.abspath(os.curdir)
+        self.data = pd.DataFrame()
 
-    return pd.Series(data=model.predict(data), index=data.index)
+        return
 
+    def load_prepare_data(self, treatments_list, treatment_column, data_name):
+        """
+        This method loads the data and prepares binary columns for each value of the treatment feature
+        :param treatments_list: all possible values of treatment feature
+        :param treatment_column: name of treatment column in data
+        :param data_name: data file name
+        :return:
+        """
 
-def matching(label, propensity, calipher=0.05, replace=True):
-    """ Performs nearest-neighbour matching for a sample of test and control
-    observations, based on the propensity scores for each observation.
+        self.data = pd.read_excel(os.path.join(self.base_directory, data_name))
+        for treat in treatments_list:
+            self.data[treat] = np.where(self.data[treatment_column] == treat, 1, 0)
 
-    Arguments:
-    ----------
-        label:      Series that contains the label for each observation.
-        propensity: Series that contains the propensity score for each observation.
-        calipher:   Bound on distance between observations in terms of propensity score.
-        replace:    Boolean that indicates whether sampling is with (True) or without replacement (False).
-    """
+        return
 
-    treated = propensity[label == 1]
-    control = propensity[label == 0]
+    def estimate_propensity(self, formula, data, show_summary=False):
+        """ Estimates a propensity model using generalized linear models.
 
-    # Randomly permute in case of sampling without replacement to remove any bias arising from the
-    # ordering of the data set
-    matching_order = np.random.permutation(label[label == 1].index)
-    matches = {}
+        Arguments:
+        ----------
+            formula:       String that contains the (R style) model formula.
+            data:          DataFrame containing the covariates for each observation.
+            show_summary:  Boolean that indicates to print out the model summary to the console.
+        """
+        model = smf.glm(formula=formula, data=data, family=sma.families.Binomial()).fit()
 
-    for obs in matching_order:
-        # Compute the distance between the treatment observation and all candidate controls in terms of
-        # propensity score
-        distance = abs(treated[obs] - control)
+        if show_summary:
+            print(model.summary())
 
-        # Take the closest match
-        if distance.min() <= calipher or not calipher:
-            matches[obs] = [distance.argmin()]
-
-            # Remove the matched control from the set of candidate controls in case of sampling without replacement
-            if not replace:
-                control = control.drop(matches[obs])
-
-    return matches
+        return pd.Series(data=model.predict(data), index=data.index)
 
 
-def matching_to_dataframe(match, covariates, remove_duplicates=False):
-    """ Converts a list of matches obtained from matching() to a DataFrame.
-    Duplicate rows are controls that where matched multiple times.
+    def matching(self, label, propensity, calipher=0.05, replace=True):
+        """ Performs nearest-neighbour matching for a sample of test and control
+        observations, based on the propensity scores for each observation.
 
-    Arguments:
-    ----------
-        match:              Dictionary with a list of matched control observations.
-        covariates:         DataFrame that contains the covariates for the observations.
-        remove_duplicates:  Boolean that indicates whether or not to remove duplicate rows from the result.
-                            If matching with replacement was used you should set this to False****
-    """
-    treated = list(match.keys())
-    control = [ctrl for matched_list in match.values() for ctrl in matched_list]
+        Arguments:
+        ----------
+            label:      Series that contains the label for each observation.
+            propensity: Series that contains the propensity score for each observation.
+            calipher:   Bound on distance between observations in terms of propensity score.
+            replace:    Boolean that indicates whether sampling is with (True) or without replacement (False).
+        """
 
-    result = pd.concat([covariates.loc[treated], covariates.loc[control]])
+        treated = propensity[label == 1]
+        control = propensity[label == 0]
 
-    if remove_duplicates:
-        return result.groupby(result.index).first()
-    else:
-        return result
+        # Randomly permute in case of sampling without replacement to remove any bias arising from the
+        # ordering of the data set
+        matching_order = np.random.permutation(label[label == 1].index)
+        matches = {}
 
+        for obs in matching_order:
+            # Compute the distance between the treatment observation and all candidate controls in terms of
+            # propensity score
+            distance = abs(treated[obs] - control)
 
-def trim_common_support(data, label_name):
-    """ Removes observations that fall outside the common support of the propensity score
-        distribution from the data.
+            # Take the closest match
+            if distance.min() <= calipher or not calipher:
+                matches[obs] = [distance.argmin()]
 
-    Arguments:
-    ----------
-        data:        DataFrame with the propensity scores for each observation.
-        label_name:  Column name that contains the labels (treatment/control) for each observation.
+                # Remove the matched control from the set of candidate controls in case of sampling without replacement
+                if not replace:
+                    control = control.drop(matches[obs])
 
-    """
-    group_min_max = (data.groupby(label_name)
-                     .propensity.agg({"min_propensity": np.min, "max_propensity": np.max}))
-
-    # Compute boundaries of common support between the two propensity score distributions
-    min_common_support = np.max(group_min_max.min_propensity)
-    max_common_support = np.min(group_min_max.max_propensity)
-
-    common_support = (data.propensity >= min_common_support) & (data.propensity <= max_common_support)
-    control = (data[label_name] == 0)
-    treated = (data[label_name] == 1)
-
-    return data[common_support]
+        return matches
 
 
-def qq_plot(x, y, variable_name):
-    """ Produces a QQ-plot where the percentiles of two empirical distributions are compared against each other.
+    def matching_to_dataframe(self, match, covariates, remove_duplicates=False):
+        """ Converts a list of matches obtained from matching() to a DataFrame.
+        Duplicate rows are controls that where matched multiple times.
 
-    Arguments:
-    ----------
-        x:              Vector with samples from the first distribution.
-        y:              Vector with samples from the second distribution.
-        variable_name:  Name of the variable.
+        Arguments:
+        ----------
+            match:              Dictionary with a list of matched control observations.
+            covariates:         DataFrame that contains the covariates for the observations.
+            remove_duplicates:  Boolean that indicates whether or not to remove duplicate rows from the result.
+                                If matching with replacement was used you should set this to False****
+        """
+        treated = list(match.keys())
+        control = [ctrl for matched_list in match.values() for ctrl in matched_list]
 
-    """
-    q = np.arange(0, 100)
-    a = np.percentile(a=x, q=q)
-    b = np.percentile(a=y, q=q)
+        result = pd.concat([covariates.loc[treated], covariates.loc[control]])
 
-    fig, ax = plt.subplots(figsize=(8, 8))
-    plt.title("QQ-plot of variable %s" % variable_name)
+        if remove_duplicates:
+            return result.groupby(result.index).first()
+        else:
+            return result
 
-    plt.plot(a, b, 'o')
-    plt.plot([0, np.max(a)], [0, np.max(a)], '-')
 
-    ax.set_xlabel("Control quantiles")
-    ax.set_ylabel("Treatment quantiles")
+    def trim_common_support(self, data, label_name):
+        """ Removes observations that fall outside the common support of the propensity score
+            distribution from the data.
+
+        Arguments:
+        ----------
+            data:        DataFrame with the propensity scores for each observation.
+            label_name:  Column name that contains the labels (treatment/control) for each observation.
+
+        """
+        group_min_max = (data.groupby(label_name)
+                         .propensity.agg({"min_propensity": np.min, "max_propensity": np.max}))
+
+        # Compute boundaries of common support between the two propensity score distributions
+        min_common_support = np.max(group_min_max.min_propensity)
+        max_common_support = np.min(group_min_max.max_propensity)
+
+        common_support = (data.propensity >= min_common_support) & (data.propensity <= max_common_support)
+        control = (data[label_name] == 0)
+        treated = (data[label_name] == 1)
+
+        return data[common_support]
+
+
+    def qq_plot(self, x, y, variable_name):
+        """ Produces a QQ-plot where the percentiles of two empirical distributions are compared against each other.
+
+        Arguments:
+        ----------
+            x:              Vector with samples from the first distribution.
+            y:              Vector with samples from the second distribution.
+            variable_name:  Name of the variable.
+
+        """
+        q = np.arange(0, 100)
+        a = np.percentile(a=x, q=q)
+        b = np.percentile(a=y, q=q)
+
+        fig, ax = plt.subplots(figsize=(8, 8))
+        plt.title("QQ-plot of variable %s" % variable_name)
+
+        plt.plot(a, b, 'o')
+        plt.plot([0, np.max(a)], [0, np.max(a)], '-')
+
+        ax.set_xlabel("Control quantiles")
+        ax.set_ylabel("Treatment quantiles")
+
+
+def main():
+
+    matching = Matching()
+    treatments_list = ['positive', 'negative']
+    treatment_column = 'treated'
+    data_name = 'final_features_causality.xlsx'
+    #column_name = 'propensity_score_' + treatment + '_' + method
+    variable_name = ['comment_author_number_original_subreddit', 'comment_author_number_recommend_subreddit',
+                     'percent_efficient_references_comment_author', 'number_of_references_comment_author',
+                     'number_of_efficient_references_comment_author', 'submission_author_number_original_subreddit',
+                     'number_of_inefficient_references_comment_author', 'subreddits_similarity',
+                     'submission_author_number_recommend_subreddit', 'cosine_similarity_subreddits_list',
+                     'comment_created_time_hour', 'submission_created_time_hour', 'time_between_messages',
+                     'comment_len', 'number_of_r', 'comment_submission_similarity', 'comment_title_similarity',
+                     'number_of_references_to_submission', 'number_of_references_to_recommended_subreddit']
+
+    # load data and create treatment columns
+    matching.load_prepare_data(treatments_list, treatment_column, data_name)
+
+    
+if __name__ == '__main__':
+    main()
