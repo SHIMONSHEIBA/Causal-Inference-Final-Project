f2223f59570ee6d2f6ddbec0204d2fa442c55760
ssheiba <shimon.sheiba@intel.com>
2018-03-01 20:00:40 +0200

    REDDIT API - working on changemyview

--- reddit_api.py
+++ reddit_api.py
@@ -1,17 +1,35 @@
-# import requests
-# import json
 import praw
 import time
 import csv
-import pandas as pd
+import pickle
+import logging
+import os
+from datetime import datetime
 
 
 class ApiConnection:
 
-    def __init__(self):
+    def __init__(self, subreddit):
+
+        # configurate API connection details
         user_agent = "learning API 1.0"
         client_id = 'learning API 1.0'
-        self.r_connection = praw.Reddit(client_id=client_id, user_agent=user_agent, client_secret="CLIENT_SECRET")
+        user = 'ssheiba'
+        password = 'Angui100'
+        ssheiba_client_id = 'ysLnQ_KXpkYA1g'
+        ssheiba_client_secret = 'pLMhOt7sVy6IHdx5mcCy48yB6Ow'
+        ssheiba_user_agent = 'learningapp:com.example.myredditapp:v1.2.3 (by /u/ssheiba)'
+        self.r_connection = praw.Reddit(client_id=ssheiba_client_id, user_agent=ssheiba_user_agent,
+                                        client_secret=ssheiba_client_secret, username=user,password=password)
+        self.subreddit_name = subreddit
+
+        # configurate logging
+        base_directory = os.path.abspath(os.curdir)
+        run_dir = datetime.now().strftime("importing_change_my_view_%d_%m_%Y_%H_%M_%S")
+        self.directory = os.path.join(base_directory, run_dir)
+        LOG_FILENAME = datetime.now().strftime(os.path.join(self.directory,'LogFile.log'))
+        logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO)
+
         return
 
     def get_karma(self, user_id):
@@ -24,75 +42,127 @@ class ApiConnection:
             karma_by_subreddit[subreddit] = (karma_by_subreddit.get(subreddit, 0)
                                              + thing.score)
 
-    def subreddit(self, post_limit):
-        subreddit = self.r_connection.get_subreddit()
-        subreddit_posts = subreddit.get_hot(limit=post_limit)
+    def get_submissions(self):
+
+        num_of_total_submissions = 0
+        submissions = list()
+        with open('all submissions.csv', 'a') as file:
+            writer = csv.writer(file, lineterminator='\n')
+            fieldnames = ['submission_author','submission_title', 'submission_comments_by_id', 'submission_created_utc',
+                          'submission_edited', 'submission_body', 'submission_id', 'submission_likes',
+                          'submission_ups', 'submission_downs', 'submission_score','submission_num_reports',
+                          'submission_gilded','submission_distinguished', 'submission_is_crosspostable'
+                           'submission_banned_by', 'submission_banned_at_utc', 'submission_removal_reason',
+                          'submission_clicked', 'submission_num_comments','submission_contest_mode', 'submission_media']
+            writer.writerow(fieldnames)
         subids = set()
-        for submission in subreddit_posts:
+        for submission in self.r_connection.subreddit(self.subreddit_name).top('all'):
+            with open('all submissions.csv', 'a') as file:
+                writer = csv.writer(file, lineterminator='\n')
+                writer.writerow([submission.author,submission.title, submission._comments_by_id, submission.created_utc,
+                          submission.edited, submission.selftext, submission.id, submission.likes,
+                          submission.ups, submission.downs, submission.score,submission.num_reports,
+                          submission.gilded,submission.distinguished, submission.is_crosspostable,
+                          submission.banned_by, submission.banned_at_utc, submission.removal_reason,
+                          submission.clicked, submission.num_comments, submission.contest_mode, submission.media])
+
             subids.add(submission.id)
+            submissions.append(submission)
+            num_of_total_submissions +=1
+            print("added submission id : {}".format(submission.id))
+            print("total number of submissions so far is {}".format(num_of_total_submissions))
+            logging.info("added submission id : {}".format(submission.id))
+            logging.info("total number of submissions so far is {}".format(num_of_total_submissions))
         subid = list(subids)
+        print("all subids are: {}".format(subid))
+        logging.info("all subids are: {}".format(subid))
+        # save all submissions object
+        print("saving submissions list")
+        logging.info("saving submissions list")
+        with open('submissions.pickle', 'wb') as handle:
+            pickle.dump(submissions, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
         return subid
 
-    def parse_comments(self, post_limit, subid, string_to_find):
-        comments = []
+    def parse_comments(self, subid):
+        """
+        this method retrieves all comments for the subid list (and their replies)
+        :param subid: list of submissions id to get their comment tree
+        :return: saves features of the comments and replies, among with the data objects of them.
+        """
+        comments = dict()
+        num_of_total_comments = 0
         index = len(subid)
+
+        # prepare the file
+        with open('all submissions comments.csv', 'a') as file:
+            writer = csv.writer(file, lineterminator='\n')
+            fieldnames2 = ['comment_author', 'comment_created_utc', 'comment_edited', 'comment_body',
+                           'comment_path', 'comment_id', 'parent_id', 'submission_id', 'comment_is_submitter',
+                           'comment_likes', 'comment_is_root', 'comment_ups', 'comment_downs', 'comment_score',
+                           'comment_num_reports','comment_gilded','comment_distinguished', 'comment_controversiality'
+                           'comment_banned_by', 'comment_banned_at_utc', 'comment_depth', 'comment_removal_reason']
+            writer.writerow(fieldnames2)
+
+        # iterate all submissions
         for i in range(0, index):
             print('{}: start submission {} for id {}'.format((time.asctime(time.localtime(time.time()))), i, subid[i]))
+            logging.info('{}: start submission {} for id {}'.format((time.asctime(time.localtime(time.time()))), i, subid[i]))
             submission = self.r_connection.submission(id=subid[i])
             print('{}: start more comments'.format((time.asctime(time.localtime(time.time())))))
-            submission.replace_more_comments(limit=None, threshold=0)
-            print('{}: start flat comments'.format((time.asctime(time.localtime(time.time())))))
-            flat_comments = praw.helpers.flatten_tree(submission.comments)
-            # with open('alreadydone.txt', 'r') as f:
-            #     already_done = [line.strip() for line in f]
-            # f.close()
-            for comment in flat_comments:
-                # if comment.id==u'da7f3xh':
-                #     r=1
-                #if string_to_find in comment.body:
-                    # reference_subreddit = comment.body[comment.body.find(string_to_find):].split(
-                    #     ('/'))  # split the body from /r/
-                    # reference_subreddit = reference_subreddit[2][:reference_subreddit[2].find(' ')].encode(
-                    #     'utf-8')  # find the subreddit name in the reference
-                    # reference_subreddit = [reference_subreddit]
-                    # if 'edit' not in comment.body:
-                    #     if 'I am a bot, and this action was performed automatically' not in comment.body:
-                    #         if string_to_find + self.sub_reddit not in comment.body:
-                    #             if reference_subreddit in self.subreddits_list:
-                comments.append(comment)
-                with open('all submissions and comments.csv', 'w') as file:
+            logging.info('{}: start more comments'.format((time.asctime(time.localtime(time.time())))))
+
+            # replace the more comments objects with the comments themselves (flatten trees of replies of comments)
+            submission.comment_sort = 'new'
+            submission.comments.replace_more(limit=None)
+
+            comments[subid[i]] = list()
+
+            # retrieve all comments data
+            for comment in submission.comments.list():
+
+                comments[subid[i]].append(comment)
+                with open('all submissions and comments.csv', 'a') as file:
                     writer = csv.writer(file, lineterminator='\n')
-                    fieldnames2 = ['comment_body', 'comment_path', 'comment_id', 'parent_id', 'submission_id']
-                    writer.writerow(fieldnames2)
-                    # for comment in reference_comments:
-                    writer.writerow([comment.body.encode('utf-8'), comment.permalink.encode('utf-8'),
+                    writer.writerow([comment.author,comment.created_utc,
+                                     comment.edited,
+                                     comment.body.encode('utf-8'), comment.permalink.encode('utf-8'),
                                      comment.id.encode('utf-8'), comment.parent_id.encode('utf-8'),
-                                     comment.submission.id.encode('utf-8')])
-                                    # real_comments = [comment for comment in flat_comments if string_to_find in comment.body]
-                                    # reference_comments += real_comments
-            print("number of comments for subid {} is {}".format(subid[i], len(comments)))
-        # with open(self.sub_reddit + ' reference comments.csv', 'w') as file:
-        #     writer = csv.writer(file, lineterminator='\n')
-        #     fieldnames2 = ['comment_body', 'comment_path', 'comment_id', 'parent_id', 'submission_id']
-        #     writer.writerow(fieldnames2)
-        #     # for comment in reference_comments:
-        #     writer.writerow([comment.body.encode('utf-8'), comment.permalink.encode('utf-8'),
-        #                      comment.id.encode('utf-8'), comment.parent_id.encode('utf-8'),
-        #                      comment.submission.id.encode('utf-8')])
+                                     comment.submission.id.encode('utf-8'),comment.is_submitter,
+                                     comment.likes, comment.is_root, comment.ups, comment.downs, comment.score,
+                                     comment.num_reports, comment.gilded, comment.distinguished, comment.controversiality,
+                                     comment.banned_by, comment.banned_at_utc, comment.depth, comment.removal_reason])
+            print("number of comments for subid {} is {}".format(subid[i], len(comments[subid[i]])))
+            logging.info("number of comments for subid {} is {}".format(subid[i], len(comments[subid[i]])))
+            num_of_total_comments += len(comments[subid[i]])
+            print("total number of comments so far is {}".format(num_of_total_comments))
+            logging.info("total number of comments so far is {}".format(num_of_total_comments))
+
+        # save all comments object
+        with open('comments.pickle', 'wb') as handle:
+            pickle.dump(comments, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
         return
 
 
 def main():
-    post_limit = 3000
-    string_to_find = '/r/'
-    sub_reddit = "diet"
-    print('Run with {} hot submissions for sub reddit {}'.format(post_limit, sub_reddit))
-    connect = ApiConnection()
-    # subid = connect.subreddit(post_limit)
-    subid = pd.read_excel("submission_ids.xlsx")
-    subid = subid["submission_id"].tolist()
-    connect.parse_comments(post_limit, subid, string_to_find)
+
+    subreddit = 'changemyview'
+    print('{} : Run for sub reddit {}'.format((time.asctime(time.localtime(time.time()))), subreddit))
+    logging.info('{} : Run for sub reddit {}'.format((time.asctime(time.localtime(time.time()))), subreddit))
+    # create class instance
+    connect = ApiConnection(subreddit)
+
+    # get submissions of sub reddit
+    subids = connect.get_submissions()
+    # get comments of submissions
+    # subid = pd.read_excel("submission_ids_test.xlsx")
+    # subid = subid["submission_id"].tolist()
+    connect.parse_comments(subids)
+    print('{} : finished Run for sub reddit {}'.format((time.asctime(time.localtime(time.time()))), subreddit))
+    logging.info('{} : finished Run for sub reddit {}'.format((time.asctime(time.localtime(time.time()))), subreddit))
 
 if __name__ == '__main__':
     main()
+
+
