8b9366d85cd57047e6e13b8d7db0f2c81c93fb6b
ssheiba <shimon.sheiba@intel.com>
2018-04-06 14:20:57 +0300

    working on CNN

--- install_nltk.py
+++ install_nltk.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import pickle
-from keras import backend as K
+# INSTALL:
 # conda install -c conda-forge keras
+#pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/windows/cpu/tensorflow-1.0.1-cp35-cp35m-win_amd64.whl
+
 from keras.layers import Flatten, Dense, Input, Conv2D, Dropout
 from keras.layers import MaxPooling2D, GlobalAveragePooling2D
 from keras.models import Sequential
 import keras
+import os
+import pandas as pd
 
 ### CNN MODEL ###
 
@@ -17,15 +21,35 @@ class AccuracyHistory(keras.callbacks.Callback):
         self.acc.append(logs.get('acc'))
 
 ### config
-input_shape = (1,50)
+
+base_directory = os.path.abspath(os.curdir)
+results_directory = os.path.join(base_directory, 'importing_change_my_view')
+data = pd.read_csv(results_directory+"matches_data_frame_treated_propensity_score_treated_logistic.csv")
+variable_name = ['commenter_number_submission', 'submitter_number_submission',
+                 'submitter_seniority_days', 'is_first_comment_in_tree',
+                 'commenter_number_comment', 'submitter_number_comment',
+                 'number_of_comments_in_tree_by_comment_user', 'commenter_seniority_days',
+                 'time_ratio', 'comment_len', 'submission_len', 'title_len', 'time_between_messages',
+                 'time_until_first_comment', 'time_between_comment_first_comment', 'comment_depth',
+                 'number_of_comments_in_tree_from_submitter', 'number_of_respond_by_submitter_total',
+                 'respond_to_comment_user_responses_ratio', 'number_of_respond_by_submitter',
+                 'respond_to_comment_user_all_ratio', 'respond_total_ratio', 'treated', 'nltk_com_sen_pos',
+                 'nltk_com_sen_neg', 'nltk_com_sen_neutral', 'nltk_sub_sen_pos', 'nltk_sub_sen_neg',
+                 'nltk_sub_sen_neutral', 'nltk_title_sen_pos', 'nltk_title_sen_neg', 'nltk_title_sen_neutral',
+                 'nltk_sim_sen', 'percent_adj', 'submmiter_commenter_tfidf_cos_sim',
+                 'topic_model_0', 'topic_model_1', 'topic_model_2', 'topic_model_3', 'topic_model_4',
+                 'topic_model_5', 'topic_model_6', 'topic_model_7', 'topic_model_8', 'topic_model_9',
+                 'topic_model_10', 'topic_model_11', 'topic_model_12', 'topic_model_13', 'topic_model_14']
+input_shape = (1, 50)
 num_classes = 2
-batch_size = 15,000
+batch_size = 15000
 epochs = 10
-x_train = 
-y_train =
-x_test =
-y_test =
+x_train = data[variable_name]
+y_train = data["delta"]
+x_test = data[variable_name]
+y_test = data["delta"]
 history = AccuracyHistory()
+
 
 ###
 
