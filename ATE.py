6a2616ad08bac79198e627eac15cd5720120e809
reutapel <reutapel88@gmail.com>
2018-04-06 12:55:36 +0300

    clean ATE code

--- ATE.py
+++ ATE.py
@@ -6,42 +6,43 @@ import os
 from sklearn.naive_bayes import MultinomialNB
 
 
-class Ate:
+def estimate_ate(Y, X, treatment_column, model, label_column):
+    """
+    This function estimate the ATE for the treatment
+    :param pandas DF Y: the labels
+    :param pandas DF X: the covariates
+    :param str treatment_column: the name of the treatment column
+    :param model: the trained model
+    :param str label_column: the name of the label column
+    :return: the treatment effect based on the covarite adjustment method
+    """
 
-    def __init__(self, data):
+    gold_treated = Y.loc[Y[treatment_column] == 1, label_column]
+    gold_control = Y.loc[Y[treatment_column] == 0, label_column]
+    num_of_treated = gold_treated.shape[0]
+    num_of_control = gold_control.shape[0]
 
-        self.data = data
+    predicted_treated_as_control_covariates = X.loc[X[treatment_column] == 1]
+    predicted_treated_as_control_covariates[[treatment_column]] = 0
+    predicted_control_as_treated_covariates = X.loc[X[treatment_column] == 0]
+    predicted_control_as_treated_covariates[[treatment_column]] = 1
 
-        return
+    predicted_treated_as_control = model.predict(predicted_treated_as_control_covariates)
+    predicted_control_as_treated = model.predict(predicted_control_as_treated_covariates)
 
-    def estimate_ate(self, Y, X, treatment_column, model):
+    part_one = (1/num_of_treated)*sum(gold_treated - predicted_treated_as_control)
+    print("part one is: ", part_one)
+    part_two = (1/num_of_control)*sum(predicted_control_as_treated - gold_control)
+    print("part two is: ", part_two)
+    covariate_adjustment = part_one + part_two
 
-        gold_treated = Y.loc[Y[treatment_column] == 1, "delta"]
-        gold_control = Y.loc[Y[treatment_column] == 0, "delta"]
-        num_of_treated = gold_treated.shape[0]
-        num_of_control = gold_control.shape[0]
-
-        predicted_treated_as_control_covariates = X.loc[X[treatment_column] == 1]
-        predicted_treated_as_control_covariates[[treatment_column]] = 0
-        predicted_control_as_treated_covariates = X.loc[X[treatment_column] == 0]
-        predicted_control_as_treated_covariates[[treatment_column]] = 1
-
-        predicted_treated_as_control = model.predict(predicted_treated_as_control_covariates)
-        predicted_control_as_treated = model.predict(predicted_control_as_treated_covariates)
-
-        part_one = (1/num_of_treated)*sum(gold_treated - predicted_treated_as_control)
-        print("part one is: ", part_one)
-        part_two = (1/num_of_control)*sum(predicted_control_as_treated - gold_control)
-        print("part two is: ", part_two)
-        covariate_adjustment = part_one + part_two
-
-        return covariate_adjustment
+    return covariate_adjustment
 
 
 def main():
 
     model = MultinomialNB(alpha=.01)
-    treatments_list = [['treated', 'matches_data_frame_treated_propensity_score_treated_logistic.csv']]
+    treatments_list = [['treated', 'delta', 'matches_data_frame_treated_propensity_score_treated_logistic.csv']]
     variable_name = ['commenter_number_submission', 'submitter_number_submission',
                      'submitter_seniority_days', 'is_first_comment_in_tree',
                      'commenter_number_comment', 'submitter_number_comment',
@@ -60,15 +61,14 @@ def main():
     sub_directory = 'importing_change_my_view'
     for treats in treatments_list:
         treatment_column = treats[0]
-        data_name = treats[1]
+        data_name = treats[2]
         # propensity_column_name = treats[2]
         data_path = os.path.join(sub_directory, data_name)
         data = pd.read_csv(data_path)
         X = data[variable_name]
-        Y = data[['delta', treatment_column]]
-        model.fit(X=X, y=Y['delta'])
-        ate = Ate(data)
-        covariate_adjustment = ate.estimate_ate( Y, X, treatment_column, model)
+        Y = data[[treats[1], treatment_column]]
+        model.fit(X=X, y=Y[treats[1]])
+        covariate_adjustment = estimate_ate(Y, X, treatment_column, model, treats[1])
         print("covariate_adjustment ATE estimate for {} treatment is: {}".format(treatment_column, covariate_adjustment))
     return
 
