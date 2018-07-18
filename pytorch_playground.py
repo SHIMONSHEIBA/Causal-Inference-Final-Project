252936b84da5aa9b82686b7d2281078e681c4469
ssheiba <shimon.sheiba@intel.com>
2018-07-18 16:34:26 +0300

    pytorch_playground

--- pytorch_playground.py
+++ pytorch_playground.py
@@ -1,6 +1,10 @@
 import torch as tr
+import pandas as pd
 
 a = tr.rand(3,4,5)
 
 print(a)
 
+# all_submissions_comments_label = pd.read_csv(filepath_or_buffer="all_submissions_comments_with_label_all_deltalog_final.csv",
+#                                              index_col=False)
+print(a)
